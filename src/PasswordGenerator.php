7ba3a433f6c93776df4f6cd15ed494a1314f487e
Hannah Warmbier <hannahwarmbier@gmail.com>
2016-11-13 11:46:24 -0600

    Allow choosing a delimiter

--- src/PasswordGenerator.php
+++ src/PasswordGenerator.php
@@ -6,15 +6,18 @@ use SplFileObject;
 
 class PasswordGenerator
 {
-    public static function generate()
+    public static function generate($delimiter = '')
     {
-        $word = '';
+        $words = array();
+
         $file = new SplFileObject(__DIR__ . '/../resources/google-10000-english-usa.txt');
+
         for ($i = 0; $i < 4; $i++) {
             $lineNumber = random_int(0, 9999);
             $file->seek($lineNumber);
-            $word .= trim($file->current());
+            $words[] = trim($file->current());
         }
-        return $word;
+
+        return implode((string) $delimiter, $words);
     }
 }
